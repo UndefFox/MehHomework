4287b485c5431179a0eb030775aa9d9d01e55c91
UndefFox <UndefFox@gmail.com>
2024-09-20 16:55:58 +0300

    Added result to 0 homework

--- R/0/main.r
+++ R/0/main.r
@@ -1,3 +1,19 @@
 #!/usr/bin/env Rscript
 
 version;
+
+# Result:
+#platform       x86_64-pc-linux-gnu         
+#arch           x86_64                      
+#os             linux-gnu                   
+#system         x86_64, linux-gnu           
+#status                                     
+#major          4                           
+#minor          4.1                         
+#year           2024                        
+#month          06                          
+#day            14                          
+#svn rev        86737                       
+#language       R                           
+#version.string R version 4.4.1 (2024-06-14)
+#nickname       Race for Your Life    
